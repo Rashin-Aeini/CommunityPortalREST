c47bbabeca25827bf146e5fc5bd8f7791bd68454
Rashin Aeini <aeini.rashin@gmail.com>
2022-01-26 15:14:52 +0330

    Start to build Auth

---
+++ CommunityPortalREST/Models/Domains/Account.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace CommunityPortalREST.Models.Domains
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public List<Token> Tokens { get; set; }
+    }
+}
---
+++ CommunityPortalREST/Models/Domains/Token.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CommunityPortalREST.Models.Domains
+{
+    public class Token
+    {
+        public int Id { get; set; }
+        public string Number { get; set; }
+        public DateTime Expire { get; set; }
+        public int AccountId { get; set; }
+        public Account Account { get; set; }
+    }
+}
